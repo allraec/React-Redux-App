ca8eeb60a67090e125f280b872f0447828c11a51
Allison Castaneda <allraec@yahoo.com>
2021-01-20 21:53:05 -0600

    mvp

--- app/src/components/Quote.js
+++ app/src/components/Quote.js
@@ -10,6 +10,7 @@ const Quote = ({title, quote, isFetching, error, fetchQuote}) => {
     return(
         <>
             <h1>{title}</h1>
+                <img src='https://pbs.twimg.com/media/D8-U-w6UIAAHHzv?format=jpg&name=medium' width='500px' height='500px'/>
                 <h2>Taylor says: {quote}</h2>
             <button onClick={fetchQuote}>Get new Taylor quote!</button>
         </>
